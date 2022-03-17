9cadc60d7036ab718a169afc5b28f1b8b96aca5d
zangote1er <65958661+zangote1er@users.noreply.github.com>
2022-03-17 11:56:42 +0100

    create folder

---
+++ Common/extract.php
@@ -0,0 +1,15 @@
+<?php
+ $string= 
+   file_get_contents("../data.json");
+  if($string===false){
+      //deal with error...
+  }
+  $json_a=json_decode($string, true);
+  if ($json_a===false){
+      //deal with error...
+  }
+   foreach ($json_a["row"] as $person_Name=> $person_a) {
+       echo "<p> ".$person_a["Name"]. "
+       ".$person_a["Number"]."  ".$person_a["type"]." ".$person_a["Username"]." </p>";
+        }
+       ?>  
