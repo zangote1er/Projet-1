10c86711974d271ce4a0a7a5e9ef746b42e21c7d
zangote1er <65958661+zangote1er@users.noreply.github.com>
2022-03-28 09:30:41 +0100

    Connect_LDAP.
    Je dois maintenant remplir les paramètres de port et arborescence supérieure(Racine) et installer les extensions du LDAP dans la configuration de mon fichier php.

--- Controller/controlCoLdap.php
+++ Controller/controlCoLdap.php
@@ -1,4 +1,22 @@
 <?php
- echo file_get_contents("configLDAP.json");
+ // Fichier de configuration pour l'interface PHP
+   //  de notre annuaire LDAP
+   $server = "server 2012 R2";
+$port = "";
+$racine = "";
+$rootdn = "cn=ldap_admin, o=  c=fr";
+$rootpw = "Détermine@23";
+
+echo "Connexion...<br>";
+$ds= ldap_connect($server,$port);
+if ($ds==1)
+  {
+  // on s'authentifie en tant que super-utilisateur, ici, ldap_admin
+  $r= ldap_bind($ds,$rootdn,$rootpw);
+// Ici les opérations à effectuer
+}
+else {
+ echo  "Impossible de se connecter au serveur LDAP";
+}
  ?>  
    
