dda0f4e25b0f56eccf408d3b25eaa7e7d04a9aac
hubigin <hubert.mouginot@gmail.com>
2022-03-30 14:22:16 +0100

    reneame file

--- Vue/index.php
+++ Vue/index.php
@@ -1,11 +1,7 @@
 <?php
 
 include 'header.html';
-require 'conexionLDAP.html';
+require 'connexionLDAP.html';
 require 'infoInternet.html';
 include 'tableauContact.html';
 require 'bottom.html';
-<<<<<<< HEAD
-=======
-?>
->>>>>>> e3adfeac8f6cc91fc9b2fc963fead5c64130620b
