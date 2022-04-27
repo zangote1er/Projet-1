95b8de9bba4a0d6c01be88a1a06909b3fdbe5500
zangote1er <yvan.zangue@2024.ucac-icam.com>
2022-04-27 08:59:41 +0100

    Problème d'ajout d'utilisateur

--- Controller/ldapConexion.php
+++ Controller/ldapConexion.php
@@ -3,30 +3,33 @@
 $cn= htmlspecialchars($_POST['Nom']);
 $Prénom= htmlspecialchars($_POST['Prénom']);
 $Motdepasse= htmlspecialchars($_POST['Mot de passe ']);
-echo "Ajout du user: $cn" <br>;
+
 // Fichier de configuration pour l'interface PHP de notre annuaire LDAP
 
 $conexionLdap = json_decode(file_get_contents("../Config/configLDAP.json"));
-$ds = ldap_connect($conexionLdap->servername,$conexionLdap->port);
- or  die ('Could not connect to LDAP server');
+$ds = ldap_connect("192.168.2.131" , 389) or  die ('Could not connect to LDAP server');
  // on suppose que le serveur LDAP est sur le serveur local
  
 if ($ds) {
  
       // Connexion avec une identité qui permet les modifications       $ds, $cn , $conexionLdap->rootdn ,$info
-    ldap_bind($ds, $conexionLdap->rootdn);
- echo "Connected Successfully"
-    //Prépare les données
-    $info["cn"] = "$cn";
-    $info["Prénom"]="$Prénom";
-    $info["Mot de passe"]="$Motdepasse";
-    $info["objectClass"]="inetOrgPerson";
- 
-    // Ajoute les données au dossier
-$r=ldap_add(($dS, "CN=$cn , ou=Users, DC=Gestion,DC=personel,DC=Icam", $info);
-$sr= ldap_search($ds, "DC=Gestion,DC=personel,DC=Icam", "cn=$cn");
+$r= ldap_bind($ds, $conexionLdap->rootdn);
+ echo "Connected Successfully";
+   //Prépare les données
+   $info["cn"] = $cn;
+   $info["Prénom"]= $Prénom; 
+   $info["Mot de passe"]=$Motdepasse;
+   $info["objectClass"]="inetOrgPerson";
+
+   // Ajoute les données au dossier
+$r=ldap_add(( $link_identifier ="CN=Gestion-SERVER1-CA,DC=Gestion,DC=Personel,DC=Icam", $conexionLdap->port), $dn = ['CN=  , ou=Users, DC=Gestion,DC=Personel,DC=Icam'], $entry = ['cn' => '', 'Prénom' => '', 'Mot de passe' => '', 'objectClass' => 'inetOrgPerson'] );
+$sr= ldap_search(($link_identifier ="CN=Gestion-SERVER1-CA,DC=Gestion,DC=Personel,DC=Icam", $conexionLdap->port), $base_dn = 'DC=Gestion,DC=Personel,DC=Icam', $filter = 'cn=');
 $info= ldap_get_entries($ds, $sr);
- echo "L'utilisateur: <span class='result'>" $info[0] {"rootdn"} "</span> a été crée.  <br>"
+echo "L'utilisateur: <span class='result'>" . $info[0] ["rootdn"] .  "</span> a été crée.  <br>";
+
+ldap_close("192.168.2.131", $conexionLdap->port); 
+} else{
+   echo " Could not connect to LDAP server";
 }
-ldap_close($ds);
 ?>
+   
